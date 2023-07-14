cef4630d1a63db633c7793feda476eae85ae69eb
Han <hanzyong1@gmail.com>
2023-07-14 17:06:54 +0800

    initial commit

---
+++ screens/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>About</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
---
+++ screens/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
