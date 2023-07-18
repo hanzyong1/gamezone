7674f8f19c428c2dd1295cc0a623be230a0befab
Han <hanzyong1@gmail.com>
2023-07-18 19:48:33 +0800

    add form validation

--- screens/ReviewForm.js
+++ screens/ReviewForm.js
@@ -1,9 +1,27 @@
-import { Formik } from "formik";
+import { ErrorMessage, Field, Formik } from "formik";
 import React from "react";
-import { Button, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import { globalStyles } from "../styles/global";
+import * as yup from "yup";
 
 export default function ReviewForm({ addReview }) {
+  const reviewSchema = yup.object({
+    title: yup
+      .string()
+      .required("Required")
+      .min(4, "Must be at least 4 characters"),
+    body: yup
+      .string()
+      .required("Required")
+      .min(8, "Must be at least 8 characters"),
+    rating: yup
+      .string()
+      .required("Required")
+      .test("is-num-1-5", "Must be 1 - 5", (val) => {
+        return parseInt(val) < 6 && parseInt(val) > 0;
+      }),
+  });
+
   return (
     <View style={globalStyles.container}>
       <Formik
@@ -12,8 +30,16 @@ export default function ReviewForm({ addReview }) {
           addReview(values);
           actions.resetForm();
         }}
+        validationSchema={reviewSchema}
       >
-        {({ handleChange, handleSubmit, values }) => {
+        {({
+          handleChange,
+          handleSubmit,
+          handleBlur,
+          values,
+          errors,
+          touched,
+        }) => {
           return (
             <View>
               <TextInput
@@ -21,20 +47,34 @@ export default function ReviewForm({ addReview }) {
                 placeholder="Review title"
                 onChangeText={handleChange("title")}
                 value={values.title}
+                onBlur={handleBlur("title")}
               />
+              <Text style={globalStyles.errorText}>
+                {touched.title && errors.title}
+              </Text>
               <TextInput
                 style={globalStyles.input}
                 placeholder="Review details"
                 onChangeText={handleChange("body")}
                 value={values.body}
+                onBlur={handleBlur("body")}
               />
+              <Text style={globalStyles.errorText}>
+                {touched.body && errors.body}
+              </Text>
+
               <TextInput
                 style={globalStyles.input}
                 keyboardType="numeric"
                 placeholder="Rating (1-5)"
                 onChangeText={handleChange("rating")}
                 value={values.rating}
+                onBlur={handleBlur("rating")}
               />
+              <Text style={globalStyles.errorText}>
+                {touched.rating && errors.rating}
+              </Text>
+
               <Button color={"maroon"} title="Submit" onPress={handleSubmit} />
             </View>
           );
